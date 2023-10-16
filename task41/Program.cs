cb4cd28cb5fa39b26fcf51d27b8b176968b9c059
Askar <abekenov@mail.ru>
2023-10-17 00:22:25 +0600

    urok6

---
+++ task41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите количество чисел: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+Console.Write($"Введите число {i + 1}: ");
+string input = Console.ReadLine();
+double num = Convert.ToDouble(input);
+if (num > 0)
+{
+count++;
+}
+}
+Console.WriteLine($"Количество чисел больше 0: {count}");
---
+++ task43/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+double k1, b1, k2, b2;
+Console.Write("Введите значение k1: ");
+k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b1: ");
+b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+b2 = Convert.ToDouble(Console.ReadLine());
+if (k1 == k2 && b1 == b2)
+{
+Console.WriteLine("Имеют множество общих точек.");
+}
+else if (k1 == k2)
+{
+Console.WriteLine("Прямые параллельны и не имеют точки пересечения.");
+}
+else
+{
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+x = Math.Round(x, 2);
+y = Math.Round(y, 2);
+Console.WriteLine("Точка пересечения прямых: x = " + x + ", y = " + y);
+}
